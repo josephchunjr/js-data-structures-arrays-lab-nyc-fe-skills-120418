046a532f926ce8889d512e1359941c893c530618
joseph Chun <jjunior.chun@gmail.com>
2019-01-23 00:48:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution here!
 const drivers = ["Milo","Otis","Garfield"]
-const appendDriver = drivers.slice()
+
 
 function destructivelyAppendDriver(newName) {
   drivers.push(newName)
